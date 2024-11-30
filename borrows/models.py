f52c8aa27f0a5046666b4b0ce6fb5f421eba4d23
Žan Mencigar <mencigarzan@gmail.com>
2024-11-30 22:48:21 +0100

    Added borrow model

--- borrows/models.py
+++ borrows/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Borrow(models.Model):
+    STATUS_CHOICES = [
+        ('borrowed', 'Borrowed'),
+        ('returned', 'Returned'),
+        ('overdue', 'Overdue'),
+    ]
+
+    user_id = models.IntegerField()
+    book_id = models.IntegerField()
+    borrow_date = models.DateField()
+    due_date = models.DateField()
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default='borrowed',
+    )
+    extend_count = models.IntegerField()
+
+    def __str__(self):
+        return self.user_id + ' - ' + self.book_id
